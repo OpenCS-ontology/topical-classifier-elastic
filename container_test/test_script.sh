9e4c3699e877a5e63c3a37272696c9dfa8c44f70
grzegorzZ1 <lukgrzes@gazeta.pl>
2023-11-27 22:16:45 +0100

    Added tests

--- container_test/test_script.sh
+++ container_test/test_script.sh
@@ -1,3 +1,5 @@
 cp /home/container_test/test_concepts/* /home/concepts_json
-cp /home/container_test/test_ttl/* /home/output
+mkdir /home/output/csis
+mkdir /home/output/scpe
+cp /home/container_test/test_ttl/* /home/output/csis
 python3 /home/pipeline.py
